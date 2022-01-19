cde1b75f4ec4a0a4c3ae2654d43fdb34ca5c3032
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2022-01-19 23:09:15 +0300

    commit stokuretim sql

---
+++ StokTakip/StokTakip/Clasess/DbConnection.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakip.Clasess
+{
+    class DbConnection
+    {
+        #region Degisken
+        public SqlConnection sqlConn;
+        private int commantTimeout = 600;
+        #endregion
+        #region Cons
+        public DbConnection()
+        {
+            try
+            {
+                sqlConn = new SqlConnection();
+                sqlConn.ConnectionString = ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+                sqlConn.Open();
+            }
+            catch //(Exception)
+            {
+
+               
+            }
+            GC.Collect();
+        }
+           ~DbConnection()
+        {
+            try
+            {
+                if (sqlConn !=null)
+                {
+                    if (sqlConn.State !=System.Data.ConnectionState.Closed)
+                    {
+                        sqlConn.Close();
+                    }
+                    sqlConn.Dispose();
+                    sqlConn = null;
+                }
+            }
+            catch //(Exception)
+            {
+
+                
+            }
+            GC.Collect();
+        }
+        #endregion
+        #region Fnk
+        public void Close()
+        {
+            try
+            {
+                if (sqlConn !=null)
+                {
+                    if (sqlConn.State !=System.Data.ConnectionState.Closed)
+                    {
+                        sqlConn.Close();
+                    }
+                    sqlConn.Dispose();
+                    sqlConn = null;
+                }
+            }
+            catch //(Exception)
+            {
+
+            }
+            GC.Collect();
+        }
+        //---------------------------
+        public DataTable GetQuery(string prmQuery)
+        {
+            DataTable ret = null;
+
+            try
+            {
+                ret = new DataTable();
+
+                if (sqlConn.State != ConnectionState.Open)
+                    sqlConn.Open();
+
+                SqlDataAdapter sqlDataAdap = new SqlDataAdapter(prmQuery, sqlConn);
+                sqlDataAdap.SelectCommand.CommandTimeout= this.commantTimeout;
+                sqlDataAdap.Fill(ret);
+                sqlDataAdap.Dispose();
+                sqlDataAdap = null;
+            }
+            catch //(Exception)
+            {
+
+                ret = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+            return ret;
+        }
+        //--------------------------
+        public object RetStoredProc(string prmStoredProcName, List<SqlParameter> prmListSqlPar)
+        {
+            object ret = null;
+            try
+            {
+                if (sqlConn.State !=ConnectionState.Open)
+                {
+                    sqlConn.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand(prmStoredProcName, sqlConn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                for (int i = 0; i < prmListSqlPar.Count; i++)
+                {
+                    cmd.Parameters.Add(prmListSqlPar[i]);
+                }
+                ret = cmd.ExecuteScalar();
+                cmd.Dispose();
+                cmd = null;
+
+            }
+            catch //(Exception)
+            {
+
+                ret = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+            return ret;
+        }
+        #endregion
+    }
+}
---
+++ StokTakip/StokTakip/girisForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace StokTakip
+{
+    partial class girisForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.btnCikis = new DevExpress.XtraEditors.SimpleButton();
+            this.btnGiris = new DevExpress.XtraEditors.SimpleButton();
+            this.txtParola = new DevExpress.XtraEditors.TextEdit();
+            this.txtKullaniciAdi = new DevExpress.XtraEditors.TextEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtParola.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtKullaniciAdi.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(29, 70);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(62, 13);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "Kullanıcı Adı :";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(54, 102);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(37, 13);
+            this.labelControl2.TabIndex = 1;
+            this.labelControl2.Text = "Parola :";
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.labelControl3);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(377, 53);
+            this.panelControl1.TabIndex = 4;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Location = new System.Drawing.Point(95, 14);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(198, 19);
+            this.labelControl3.TabIndex = 0;
+            this.labelControl3.Text = "STOK TAKİP PROGRAMI";
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCikis.Appearance.Options.UseFont = true;
+            this.btnCikis.ImageOptions.Image = global::StokTakip.Properties.Resources.close_16x16;
+            this.btnCikis.Location = new System.Drawing.Point(160, 161);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(75, 23);
+            this.btnCikis.TabIndex = 6;
+            this.btnCikis.Text = "ÇIKIŞ";
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.Appearance.Options.UseFont = true;
+            this.btnGiris.ImageOptions.Image = global::StokTakip.Properties.Resources.apply_16x16;
+            this.btnGiris.Location = new System.Drawing.Point(160, 129);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(75, 23);
+            this.btnGiris.TabIndex = 5;
+            this.btnGiris.Text = "GİRİŞ";
+            // 
+            // txtParola
+            // 
+            this.txtParola.Location = new System.Drawing.Point(121, 99);
+            this.txtParola.Name = "txtParola";
+            this.txtParola.Size = new System.Drawing.Size(167, 20);
+            this.txtParola.TabIndex = 3;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(121, 70);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(167, 20);
+            this.txtKullaniciAdi.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 194);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.panelControl1);
+            this.Controls.Add(this.txtParola);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GİRİŞ FORMU";
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtParola.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtKullaniciAdi.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit txtKullaniciAdi;
+        private DevExpress.XtraEditors.TextEdit txtParola;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.SimpleButton btnGiris;
+        private DevExpress.XtraEditors.SimpleButton btnCikis;
+    }
+}
+

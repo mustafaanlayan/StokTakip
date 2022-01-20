058cae7c870054592cc975820ba86c1a872d916e
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2022-01-20 14:40:41 +0300

    commit form2

---
+++ StokTakip/StokTakip/GlobKayitForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace StokTakip
+{
+    partial class GlobKayitForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.btnVazgeç = new DevExpress.XtraEditors.SimpleButton();
+            this.btnKaydet = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.AppearanceCaption.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupControl1.AppearanceCaption.Options.UseFont = true;
+            this.groupControl1.AppearanceCaption.Options.UseTextOptions = true;
+            this.groupControl1.AppearanceCaption.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.groupControl1.Controls.Add(this.btnVazgeç);
+            this.groupControl1.Controls.Add(this.btnKaydet);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupControl1.Location = new System.Drawing.Point(0, 0);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(800, 70);
+            this.groupControl1.TabIndex = 0;
+            this.groupControl1.Text = "groupControl1";
+            // 
+            // btnVazgeç
+            // 
+            this.btnVazgeç.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnVazgeç.Appearance.Options.UseFont = true;
+            this.btnVazgeç.ImageOptions.Image = global::StokTakip.Properties.Resources.cancel_32x321;
+            this.btnVazgeç.Location = new System.Drawing.Point(108, 26);
+            this.btnVazgeç.Name = "btnVazgeç";
+            this.btnVazgeç.Size = new System.Drawing.Size(90, 39);
+            this.btnVazgeç.TabIndex = 1;
+            this.btnVazgeç.Text = "Vazgeç";
+            this.btnVazgeç.Click += new System.EventHandler(this.btnVazgeç_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Appearance.Options.UseFont = true;
+            this.btnKaydet.ImageOptions.Image = global::StokTakip.Properties.Resources.save_32x32;
+            this.btnKaydet.Location = new System.Drawing.Point(12, 26);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(90, 39);
+            this.btnKaydet.TabIndex = 0;
+            this.btnKaydet.Text = "Kaydet";
+            // 
+            // GlobKayitForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupControl1);
+            this.KeyPreview = true;
+            this.Name = "GlobKayitForm";
+            this.Text = "GlobKayitForm";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GlobKayitForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.SimpleButton btnVazgeç;
+        private DevExpress.XtraEditors.SimpleButton btnKaydet;
+    }
+}
---
+++ StokTakip/StokTakip/GlobKayitForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace StokTakip
+{
+    public partial class GlobKayitForm : XtraForm
+    {
+        public GlobKayitForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Olay
+         private void btnVazgeç_Click(object sender, EventArgs e)
+                {
+                    Close();
+                }
+
+        private void GlobKayitForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Escape)
+            {
+                Close();
+            }
+        }
+    }
+        #endregion
+       
+}
